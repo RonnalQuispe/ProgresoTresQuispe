1ad9d513b21c25cbf9180f590a149f95fcf23dd6
QuispeRonny <ronny.quispe@udla.edu.ec>
2025-07-08 07:22:40 -0500

    genero la carpeta models  y agrego  el ID, marca, modelo, anioF.. y placa por el moemnto sinrestricciones

---
+++ ProgresoTresQuispe/Models/Vehiculo.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+
+namespace ProgresoTresQuispe.Models
+{
+    public class Vehiculo
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+
+        public int AnioFabricacion { get; set; }
+
+        public string Placa { get; set; }
+    }
+}
